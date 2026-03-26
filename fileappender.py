75e70f97abd9cb88d783f14f788d1949cf9ef076
Ryo Shin Mendoza <qrsmendoza@tip.edu.ph>
2026-03-26 16:43:38 +0800

    Delete fileappender.py

--- fileappender.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  9 21:52:38 2026
-
-@author: Shin
-"""
-file = open("newfile2.txt", 'a')
-file.write(" and also by the programmer of course.")
-file.close()
